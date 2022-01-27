f0006eae53ccb10a574d8755d07b363aadaf60c9
Marco R <mprizzuto88@gmail.com>
2022-01-26 19:16:23 -0500

    create index.php and HTML boilerplate

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>intro to forms</title>
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+	</head>
+	<body>
+		<h1>pizza form</h1>
+	</body>
+</html>
