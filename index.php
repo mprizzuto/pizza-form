1d38d624848de30e6fcce597d01f9825281ea307
Marco R <mprizzuto88@gmail.com>
2022-01-26 21:41:12 -0500

    create form and form header

--- index.php
+++ index.php
@@ -7,6 +7,29 @@
 		<link rel="stylesheet" type="text/css" href="styles/style.css">
 	</head>
 	<body>
-		<h1>pizza form</h1>
+		<main class="exercise-main">
+			<section class="welcome">
+				<inner-column>
+					<h1>pizza party form</h1>
+
+					<h2>do you love <mark>authentic neopolitan pizza?</mark></h2>
+
+					<p>well, <em>we do too</em>. we have a party, and there <strong>will be pizza</strong>. To ensure there are enough pizzas for everyone to eat, fill out this form</p>
+
+					<form method="post" action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>">
+						<label>pizza pies
+							<input type="text" name="pizza" min="0">
+						</label>
+
+						<label>amount of people
+							<input type="text" name="people">
+						</label>
+
+						<input type="submit" name="submit">
+
+					</form>
+				</inner-column>
+			</section>
+		</main>
 	</body>
 </html>
