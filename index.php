716d77a9dfd93866db2dd89b0043a2e13a50c150
Marco R <mprizzuto88@gmail.com>
2022-01-27 00:22:42 -0500

    create form variables

--- index.php
+++ index.php
@@ -9,15 +9,28 @@
 	<body>
 		<?php 
 		include "functions.php";
+
+		// a better var_dump()
 		formatInput($_POST);
 
-		$userMessage = "";
+		$userMessage = $errorMessage = "";
+		$pizzaPies = $_POST["pizza"] ?? null;
+		$guests = $_POST["guests"] ?? null;
 
-		if ( in_array("", $_POST) ) {
-			$userMessage = "error-message";
-		} else {
-			$userMessage = "success-message";
+		// check if form is submitted
+		if($_SERVER["REQUEST_METHOD"] === "POST") {
 
+			// check array for empty values
+			if ( in_array("", $_POST) ) {
+				$themeColor = "error-theme";
+				$errorTheme = "failed-field";
+				$errorMessage = "error, required";
+				$errorField = "failed-field";
+				$userMessage = "try again";
+			} else {
+				$themeColor = "success-theme";
+				$errorTheme = "passed";
+			}
 		}
 		?>
 
@@ -27,29 +40,41 @@
 					<header>
 						<h1>pizza party form</h1>
 
-						<h2>do you love <mark>authentic neopolitan pizza?</mark></h2>
+						<h2>do you love <em>authentic neopolitan pizza?</em></h2>
 
 						<p>well, <em>we do too</em>. we have a party, and there <strong>will be pizza</strong>. To ensure there are enough pizzas for everyone to eat, fill out this form</p>
+
+						<p><mark>due to prank calls, orders of 50 or more pizza pies require a phone number</mark></p>
 					</header>
 
 					<form method="post" action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>">
 						<label>pizza pies
-							<input type="text" name="pizza" min="0">
+							<input type="number" name="pizza" min="1" value="<?=$pizzaPies?>"><span class="<?=$errorTheme?>"> <?=$errorMessage?></span>
 						</label>
 
 						<label>amount of people
-							<input type="text" name="guests" min="0">
+							<input type="number" name="guests" min="1" value="<?=$guests?>"> <span class="<?=$errorTheme?>"><?=$errorMessage?></span>
 						</label>
+
+						<?php if ($pizzaPies >= 50): ?>
+							<label>phone number
+							<input type="telephone" name="guests" min="1" value=""> <span class="<?=$errorTheme?>"><?=$userMessage?><?=$errorMessage?></span>
+						</label>
+						<?php endif; ?>
 
 						<input type="submit" name="submit">
 
 					</form>
+
+					<!-- if ($pizzaPies >= 50) {
+						//output phone number field
+					} -->
 				</inner-column>
 			</section>
 
 			<section class="results">
-				<inner-column class ="<?=$userMessage?>">
-					hshs
+				<inner-column class="<?=$themeColor?>">
+					<p><?=$userMessage?></p>
 				</inner-column>
 			</section>
 		</main>
